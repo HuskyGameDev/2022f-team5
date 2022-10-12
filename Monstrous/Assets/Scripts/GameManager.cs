6fcddc054a31ae0757ed35a7cd47342daf3e26c8
Wyatt Helzer <whelzer29@gmail.com>
2022-10-11 20:28:08 -0400

    Beginning of Game Manager

---
+++ Monstrous/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour{
+
+    public static GameManager Instance;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public enum GameState
+    {
+        Pause,
+        Victory,
+        Lose
+    }
+}
