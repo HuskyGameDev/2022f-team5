a0415f798befce13887cf519f5a59f98d599db04
Chase Bootz <cmbootz@mtu.edu>
2022-10-13 16:35:39 -0400

    Rudimentary Timer

--- Monstrous/Assets/Scripts/Timer.cs
+++ Monstrous/Assets/Scripts/Timer.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class Timer : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float timeRemaining = 10;
+    public bool timerIsRunning = false;
     void Start()
     {
-        //yet to be implemented
+        timerIsRunning = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timerIsRunning)
+        {
+            if(timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
     }
 }
