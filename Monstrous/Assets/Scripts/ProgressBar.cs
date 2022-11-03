b2b85aa2876842a904ffaf6286e74e781720b660
Jacob Brendemuehl <jkbrende@mtu.edu>
2022-11-03 01:35:58 -0400

    Exp Bar Design

--- Monstrous/Assets/Scripts/ProgressBar.cs
+++ Monstrous/Assets/Scripts/ProgressBar.cs
@@ -6,38 +6,10 @@ using UnityEngine.UI;
 public class ProgressBar : MonoBehaviour
 {
     private Slider slider;
-    private ParticleSystem particleSys;
-    public float FillSpeed = 0.5f;
-    private float targetProgress = 0;
-
-    private void Awake()
-    {
-        slider = gameObject.GetComponent<Slider>();
-        particleSys = GameObject.Find("PB_Particles").GetComponent<ParticleSystem>();
-    }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        IncrementProgress(0.75f);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(slider.value < targetProgress)
-            slider.value += FillSpeed * Time.deltaTime;
-            if(!particleSys.isPlaying)
-            particleSys.Play();
-        else
-        {
-            particleSys.Stop();
-        }
-    }
+    public int exp;
     
-
-    public void IncrementProgress(float newProgress)
+    public void UpdateProgressBar(int exp)
     {
-        targetProgress = slider.value + newProgress;
+        slider.value = exp;
     }
 }
