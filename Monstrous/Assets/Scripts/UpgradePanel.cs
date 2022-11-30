bdaaf5a18d8cece11d245517d2b20498d761237b
Chase Bootz <cmbootz@mtu.edu>
2022-11-30 18:25:00 -0500

    Upgrade tweak
    The game now pauses when the upgrade menu appears and unpauses as soon as an option is chosen

--- Monstrous/Assets/Scripts/UpgradePanel.cs
+++ Monstrous/Assets/Scripts/UpgradePanel.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpgradePanel : MonoBehaviour
 {
+    private float fixedDeltaTime;
+    public static bool Paused = false;
+
     //when this panel is awakened by the player leveling up, generate an upgrade in each slot
     void Awake()
     {
-
+        this.fixedDeltaTime = Time.fixedDeltaTime;
+        Time.timeScale = 0f;
+        Paused = true;
     }
 
     // Start is called before the first frame update
@@ -16,9 +22,21 @@ public class UpgradePanel : MonoBehaviour
         
     }
 
+    void OnEnable()
+    {
+        Time.timeScale = 0f;
+        Paused = true;
+    }
+
+    void OnDisable()
+    {
+        Time.timeScale = 1f;
+        Paused = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- Monstrous/Assets/Upgrade Scripts/DamageBuff.cs
+++ Monstrous/Assets/Upgrade Scripts/DamageBuff.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DamageBuff : MonoBehaviour
 {
-    public float damageInc = 5;
+public float damageInc = 5;
 
     public void Upgrade(GameObject tar)
     {
