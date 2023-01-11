17b722a22a818c0176427628b04d3bb135a648d6
cjdummer10 <cjdummer10@gmail.com>
2023-01-11 12:07:48 -0500

    just a bit

---
+++ Monstrous/Assets/Scripts/UpgradeLoader.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeLoader : MonoBehaviour
+{
+    //a field for the gameobject of every upgrade in the game
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public ArrayList GetUpgrades
+}
