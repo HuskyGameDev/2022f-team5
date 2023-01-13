c1194f555c69701e00aadac5773338e3d6518752
cjdummer10 <cjdummer10@gmail.com>
2023-01-12 19:26:26 -0500

    the smallest commit known to man

--- Monstrous/Assets/Scripts/UpgradeLoader.cs
+++ Monstrous/Assets/Scripts/UpgradeLoader.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class UpgradeLoader : MonoBehaviour
 {
     //a field for the gameobject of every upgrade in the game
+    public GameObject DamageUpgrade;
+    public GameObject MaxHealthUpgrade;
+    public GameObject MoveSpeedUpgrade;
+    public GameObject MoreShotsUpgrade;
+    public GameObject AttackSpeedUpgrade;
 
-
-    
     void Start()
     {
         
