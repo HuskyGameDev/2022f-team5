934120e470a710c24ce2520be552f49eda0c31d8
nc5432 <ncasey113@gmail.com>
2023-01-18 13:48:49 -0500

    Started working on getting the Perlin Noise level generator working

--- Monstrous/Assets/Level Generation/Scripts/Generator/LevelGenerator.cs
+++ Monstrous/Assets/Level Generation/Scripts/Generator/LevelGenerator.cs
@@ -18,8 +18,18 @@ namespace Monstrous.Generation{
 
         public void generate(){
             System.Random prng = new System.Random(seed);
+            float offsetX = prng.Next(-100000, 100000);
+            float offsetY = prng.Next(-100000, 100000);
             foreach (Transform loc in tiles){
-                Instantiate(data.floorTiles[Random.Range(0, data.floorTiles.Length)], loc.position, Quaternion.identity, transform);
+                float noise = Mathf.PerlinNoise((loc.position.x + offsetX) * 5, (loc.position.y + offsetY) * 5);
+                if (noise > 0.6f){
+                    Instantiate(data.floorTiles[2], loc.position, Quaternion.identity, transform);
+                }
+                if (noise > 0.4f){
+                    Instantiate(data.floorTiles[1], loc.position, Quaternion.identity, transform);
+                }else{
+                    Instantiate(data.floorTiles[0], loc.position, Quaternion.identity, transform);
+                }
             }
         }
     }
