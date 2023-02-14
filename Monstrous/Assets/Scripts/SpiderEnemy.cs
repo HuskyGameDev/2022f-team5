6edccde35c505ad1e3e762dafc45724a5fd72550
jrsnowde <90734641+jrsnowde@users.noreply.github.com>
2023-02-14 16:55:02 -0500

    Update SpiderEnemy.cs

--- Monstrous/Assets/Scripts/SpiderEnemy.cs
+++ Monstrous/Assets/Scripts/SpiderEnemy.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class SpiderEnemy : Enemy
 { 
-    public int counter;
+  
    
     // Start is called before the first frame update
     void Start()
@@ -15,19 +15,5 @@ public class SpiderEnemy : Enemy
        
     }
 
-    /**
-    void FixedUpdate(){
-        counter++;
-        if(counter > 499){
-            health = health * 1.01f;
-            speed = speed*1.01f;
-            contactDamage = contactDamage*1.1f;
-            counter=0;
-            
-        }
-        Debug.Log(counter);
-        Debug.Log(health);
-    }
-**/
 }
 
