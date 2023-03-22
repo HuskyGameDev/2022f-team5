ad8df8124393450625f9d5e61b951c487a062689
cjdummer10 <cjdummer10@gmail.com>
2023-03-22 14:50:01 -0400

    Enemy Look
    enemies will face towards the player while moving

--- Monstrous/Assets/Scripts/FollowPlayer.cs
+++ Monstrous/Assets/Scripts/FollowPlayer.cs
@@ -6,12 +6,14 @@ public class FollowPlayer : MonoBehaviour
 {
 
     private float moveSpeed = 2;
+    private SpriteRenderer sprite;
     Transform target;
 
     //
     private void Awake()
     {
         target = GameObject.Find("Player").GetComponent<Transform>();
+        sprite = this.GetComponent<SpriteRenderer>();
         //moveSpeed = SpiderEnemy.speed;
     }
 
@@ -24,6 +26,14 @@ public class FollowPlayer : MonoBehaviour
     void FixedUpdate()
     {
         //update the position of the enemy to be closer to the player
+        if(target.position.x>transform.position.x)
+        {
+            sprite.flipX = true;
+        }
+        else
+        {
+            sprite.flipX = false;
+        }
         transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.fixedDeltaTime);
     }
 
