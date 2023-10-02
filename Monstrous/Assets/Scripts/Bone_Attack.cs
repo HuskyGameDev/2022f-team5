9c13756dd929237aa4e5fd55fb4ffbc8922777b5
cjdummer10 <cjdummer10@gmail.com>
2023-10-02 09:43:17 -0400

    Slerping the bonemerang
    stable but probably not working
    do not delete my tempTar pls
    i have to go to class

--- Monstrous/Assets/Scripts/Bone_Attack.cs
+++ Monstrous/Assets/Scripts/Bone_Attack.cs
@@ -7,24 +7,28 @@ using Monstrous.AI;
 public class Bone_Attack : MonoBehaviour
 {
     public Rigidbody2D proj;
+    public Transform target;
+    private Vector3 start;
     private Vector2 direction;
     [SerializeField] private Camera mainCamera;
-
-    public float speed = 20;
     public float damage;
-
     public GameObject projectileBreak;
+    private delegate void MoveBoomerang();
+    private float positionCalc = 0;
 
     //Awake
     void Awake()
     {
         damage = GameObject.FindWithTag("Player").GetComponent<Weapons>().boneAttackBaseDam;
         mainCamera = Camera.main;
+        start = transform.position;
         Vector2 temp = mainCamera.ScreenToWorldPoint(Input.mousePosition);
         direction = temp - proj.position;
         direction.Normalize();
         LookAt2D(transform, temp);
         transform.Rotate(0f, 0f, -90f, Space.Self);
+        target = GameObject.FindWithTag("Temp").GetComponent<Transform>();
+        //MoveBoomerang = MoveThrow();
     }
 
     // Start is called before the first frame update
@@ -36,8 +40,9 @@ public class Bone_Attack : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        proj.MovePosition(proj.position + direction * speed * Time.fixedDeltaTime);
-        //rotate some amount
+        positionCalc += 0.2f * Time.deltaTime;
+        transform.Rotate(0.0f, 0.0f, 3.0f, Space.Self);
+        // if arrived switch move method to return
     }
 
     private void OnTriggerEnter2D(Collider2D collided){
@@ -55,5 +60,17 @@ public class Bone_Attack : MonoBehaviour
         var direction = target - current;
         var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    //boomerang will start by moving away
+    private void MoveThrow()
+    {
+        transform.position = Vector3.Slerp(start, target.position, positionCalc);
+    }
+
+    //after reaching the target, instead move back to the player
+    private void MoveReturn()
+    {
+
     }
 }
