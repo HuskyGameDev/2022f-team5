f792fc63973a0d0ff661317de9822bcc17588844
nc5432 <ncasey113@gmail.com>
2023-10-08 16:55:06 -0400

    Improved beetle AI and stats
    - Beetle is a bit faster
    - Beetle uncurls after you move away
    - Beetle gets health boost when curled
    - Beetle now curls based on distance instead of time
    - Fixed bonemarang

--- Monstrous/Assets/Scripts/AI/BeetleAI.cs
+++ Monstrous/Assets/Scripts/AI/BeetleAI.cs
@@ -4,22 +4,37 @@ using UnityEngine;
 
 namespace Monstrous.AI{
     public class BeetleAI : EnemyBase{
+        [SerializeField] private Sprite uncurledSprite;
         [SerializeField] private Sprite curledSprite;
+        [SerializeField] private float curledHealthBoost = 1.5f;
+        [SerializeField] private float uncurlDist = 15f;
+        [SerializeField] private float minCurlDist = 2f;
+        [SerializeField] private float maxCurlDist = 7f;
+        [SerializeField] private float minCurlTime = 5f;
         bool mode = false;
+        float timer;
+        float curlDist;
+
         void Awake(){
-            StartCoroutine(curl());
+            curlDist = Random.Range(minCurlDist, maxCurlDist);
         }
 
         void FixedUpdate(){
             if (!mode){
                 transform.position = Vector2.MoveTowards(transform.position, playerLoc.position, speed * Time.fixedDeltaTime);
+                if (Vector3.Distance(transform.position, playerLoc.position) <= curlDist){
+                    mode = true;
+                    renderer.sprite = curledSprite;
+                    health *= curledHealthBoost;
+                    timer = minCurlTime;
+                }
+            }else if ((timer <= 0) && (Vector3.Distance(transform.position, playerLoc.position) > uncurlDist)){
+                mode = false;
+                renderer.sprite = uncurledSprite;
+                health /= curledHealthBoost;
+            }else{
+                timer -= Time.fixedDeltaTime;
             }
-        }
-
-        private IEnumerator curl(){
-            yield return new WaitForSeconds(Random.Range(3f, 10f));
-            mode = true;
-            renderer.sprite = curledSprite;
         }
 
         public override void onAttack(){}
--- Monstrous/Assets/Scripts/Bone_Attack.cs
+++ Monstrous/Assets/Scripts/Bone_Attack.cs
@@ -7,7 +7,7 @@ using Monstrous.AI;
 public class Bone_Attack : MonoBehaviour
 {
     public Rigidbody2D proj;
-    private Transform target;
+    private Vector3 target;
     private Vector3 start;
     private Vector2 direction;
     [SerializeField] private Camera mainCamera;
@@ -20,7 +20,7 @@ public class Bone_Attack : MonoBehaviour
     private bool returning = false;
 
     //Awake
-    void Awake()
+    void Start()
     {
         damage = GameObject.FindWithTag("Player").GetComponent<Weapons>().boneAttackBaseDam;
         player = GameObject.FindWithTag("Player").GetComponent<Transform>();
@@ -36,19 +36,13 @@ public class Bone_Attack : MonoBehaviour
         target = direction * 25.0f;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void FixedUpdate()
     {
         positionCalc += 0.8f * Time.deltaTime;
         transform.Rotate(0.0f, 0.0f, 12.0f, Space.Self);
         moveMethod();
-        if((Vector3.Distance(transform.position, target.position) <= 1f) && !returning )
+        if((Vector3.Distance(transform.position, target) <= 1f) && !returning )
         {
             moveMethod = MoveReturn;
             positionCalc = 0f;
@@ -72,12 +66,12 @@ public class Bone_Attack : MonoBehaviour
     //boomerang will start by moving away
     private void MoveThrow()
     {
-        transform.position = Vector3.Lerp(start, target.position, positionCalc);
+        transform.position = Vector3.Lerp(start, target, positionCalc);
     }
 
     //after reaching the target, instead move back to the player
     private void MoveReturn()
     {
-        transform.position = Vector3.Lerp(target.position, player.position, positionCalc);
+        transform.position = Vector3.Lerp(target, player.position, positionCalc);
     }
 }
